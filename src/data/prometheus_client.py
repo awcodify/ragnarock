f053e34ff1002ce122e36eb7d0c697ff66846c48
awcodify <awcodify@gmail.com>
2024-11-12 16:36:51 +0700

    make query more general, remove the instance name regex

--- src/data/prometheus_client.py
+++ src/data/prometheus_client.py
@@ -17,8 +17,8 @@ class PrometheusClient:
         start_time = end_time - timedelta(seconds=time_range)
         
         metrics = {
-            'cpu_usage': 'sum(rate(node_cpu_seconds_total{mode="user", instance=~".*swarm.*"}[5m])) by (instance)',
-            'memory_usage': 'sum by(instance) (node_memory_MemTotal_bytes{instance=~".*swarm.*"}) - sum by(instance)  (node_memory_MemAvailable_bytes{instance=~".*swarm.*"})',
+            'cpu_usage': 'sum(rate(node_cpu_seconds_total{mode="user"}[5m])) by (instance)',
+            'memory_usage': 'node_memory_MemTotal_bytes - node_memory_MemAvailable_bytes',
             'disk_usage': 'node_filesystem_size_bytes - node_filesystem_free_bytes',
             'network_receive': 'rate(node_network_receive_bytes_total[5m])',
             'network_transmit': 'rate(node_network_transmit_bytes_total[5m])'
